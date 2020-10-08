da6046184386183c88b10202c5a76ac45bc50686
awb123123 <awb2944210230@163.com>
2020-10-08 17:32:10 +0800

    diyici

---
+++ carmall-consumer/src/main/java/com/ytc/CarmallConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.ytc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class CarmallConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmallConsumerApplication.class, args);
+    }
+
+}
---
+++ carmall-eureka/src/main/java/com/ytc/CarmallEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.ytc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CarmallEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarmallEurekaApplication.class, args);
+    }
+
+}
