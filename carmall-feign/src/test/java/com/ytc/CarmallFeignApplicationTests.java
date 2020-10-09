eb0b24f2a1be8212b28e4deafdbf2a60162a509d
awb123123 <awb2944210230@163.com>
2020-10-09 08:44:35 +0800

    dierci

--- carmall-feign/src/test/java/com/ytc/CarmallFeignApplicationTests.java
+++ carmall-feign/src/test/java/com/ytc/CarmallFeignApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CarmallConsumerApplicationTests {
+public class CarmallFeignApplicationTests {
 
     @Test
     public void contextLoads() {
